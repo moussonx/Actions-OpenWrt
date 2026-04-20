484f50f10f4e276abd5e4077d28b4f3562862123
Yiran Wang <98829951+moussonx@users.noreply.github.com>
2026-04-20 13:23:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 1. 在脚本运行阶段直接注入 Passwall2 和插件源
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall.git;main' >> feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git;main' >> feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small.git;main' >> feeds.conf.default
 
-# Modify default theme
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+# 2. 修改默认IP为 192.168.1.2 (避开华为 F50 的 1.1)
+sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-# Modify hostname
-#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+# 3. 强制更新 Feeds (确保刚才注入的源生效)
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+# 4. 暴力删除导致编译卡死的冲突包 (23.05 版本必做)
+rm -rf feeds/packages/lang/python/micropython
+
+# 5. 针对 DS925+ VMM 虚拟机的底层优化
+echo "CONFIG_VIRTIO=y" >> .config
+echo "CONFIG_VIRTIO_NET=y" >> .config
+echo "CONFIG_VIRTIO_BLK=y" >> .config
+
+# 6. 针对 500GB SSD 的寿命保养
+echo "CONFIG_PACKAGE_fstrim=y" >> .config
+
+# 7. 提升抢票响应的内核优化
+echo "CONFIG_DEFAULT_TCP_CONG=\"bbr\"" >> .config
