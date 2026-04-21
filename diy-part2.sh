fc6a3ac9b45fdf2c181c0dd193c4560bea1901a3
Yiran Wang <98829951+moussonx@users.noreply.github.com>
2026-04-21 09:08:49 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,11 +3,15 @@
 # 1. 基础配置：修改默认 IP 为 192.168.1.2
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-# 2. 解决冲突：物理删除 feeds 里的旧插件，防止它们“抢戏”
+# 2. 解决冲突：物理删除 feeds 里的旧插件
 rm -rf feeds/luci/applications/luci-app-passwall*
 rm -rf feeds/luci/applications/luci-app-filebrowser
 rm -rf feeds/luci/applications/luci-app-lucky
 rm -rf feeds/packages/net/haproxy
+
+# ================== 🚑 核心抢救：抓内鬼补丁 ==================
+# 修复1：强降 rpcd-mod-luci 的 CMake 版本要求 (解决最后的 Error 2 致命报错)
+find feeds/luci/ -name "CMakeLists.txt" -exec sed -i 's/cmake_minimum_required(VERSION 3.31)/cmake_minimum_required(VERSION 3.25)/g' {} \;
 
 # 3. 物理注入超级版源码 (PassWall 2)
 mkdir -p package/community
@@ -15,6 +19,10 @@ curl -L https://github.com/Openwrt-Passwall/openwrt-passwall2/archive/refs/tags/
 unzip -q pw2.zip
 mv openwrt-passwall2-26.4.20-1/luci-app-passwall2 package/community/
 rm -rf pw2.zip openwrt-passwall2-26.4.20-1
+
+# 修复2：剔除 PassWall2 中的 tuic-client 依赖，防止编译 Rust 导致内存爆炸 (解决 rust 报错)
+sed -i '/tuic-client/d' package/community/luci-app-passwall2/Makefile
+# ==============================================================
 
 # 4. 拉取其他天花板组件 (iStore + Lucky + FileBrowser)
 git clone --depth=1 https://github.com/linkease/istore.git package/community/istore
