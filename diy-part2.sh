fc950e9a8336cb75c0ae2e23067b715be059894d
Yiran Wang <98829951+moussonx@users.noreply.github.com>
2026-04-23 19:01:21 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # 修改默认 IP 为你喜欢的网段 (比如 192.168.1.67)
 sed -i 's/192.168.1.1/192.168.1.67/g' package/base-files/files/bin/config_generate
+
+# 强制降低所有插件的 CMake 版本要求，完美适配 OpenWrt 内部工具链 (3.26.4)
+find feeds/ -type f -name "CMakeLists.txt" -exec sed -i 's/cmake_minimum_required.*/cmake_minimum_required(VERSION 3.20)/g' {} \;
