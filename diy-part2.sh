3d8050e01c5760740b3eba8548e1065087cc7353
Yiran Wang <98829951+moussonx@users.noreply.github.com>
2026-04-24 19:14:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -2,5 +2,6 @@
 # 修改默认 IP 为你喜欢的网段 (比如 192.168.1.67)
 sed -i 's/192.168.1.1/192.168.1.67/g' package/base-files/files/bin/config_generate
 
-# 强制降低所有插件的 CMake 版本要求，完美适配 OpenWrt 内部工具链 (3.26.4)
-find feeds/ -type f -name "CMakeLists.txt" -exec sed -i 's/cmake_minimum_required.*/cmake_minimum_required(VERSION 3.20)/g' {} \;
+# 🟢 [精准修复] 优雅降级 CMake 版本要求，解决部分插件编译报 CMake 版本过低的问题
+# 相比直接替换一整行，这里使用正则表达式仅精准替换版本号部分，绝不破坏原有语法结构 (如 project 声明或范围语法)
+find feeds/ -type f -name "CMakeLists.txt" -exec sed -i -E 's/cmake_minimum_required\s*\(\s*VERSION\s+[0-9\.]+(\.\.\.[0-9\.]+)?/cmake_minimum_required(VERSION 3.20/Ig' {} \;
